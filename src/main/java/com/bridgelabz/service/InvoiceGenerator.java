f287418700d2378e7aff6e6fe22a5d7d6de07fb0
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 20:25:50 +0530

    ADDED: Ability to return fixed Minimum fare.

--- src/main/java/com/bridgelabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/service/InvoiceGenerator.java
@@ -4,6 +4,7 @@ public class InvoiceGenerator {
 
     private static final int FARE_PER_KILOMETER = 10;
     private static final int FARE_PER_MINUTE = 1;
+    private static final double MINIMUM_FARE = 5;
 
     /**
      * TASK: To Generate and return based on distance and time passed as arguments.
@@ -13,6 +14,6 @@ public class InvoiceGenerator {
      */
     public double getFare(double distance, int time) {
         double fare = distance * FARE_PER_KILOMETER + time * FARE_PER_MINUTE;
-        return fare;
+        return Math.max(MINIMUM_FARE, fare);
     }
 }
