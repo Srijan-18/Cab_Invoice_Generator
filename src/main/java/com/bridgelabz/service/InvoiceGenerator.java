e57761cc0b809bed959290a4087a9a9f22a9f719
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 20:16:28 +0530

    ADDED: Ability to generate fare according to distance and time given.

---
+++ src/main/java/com/bridgelabz/service/InvoiceGenerator.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.service;
+
+public class InvoiceGenerator {
+
+    private static final int FARE_PER_KILOMETER = 10;
+    private static final int FARE_PER_MINUTE = 1;
+
+    /**
+     * TASK: To Generate and return based on distance and time passed as arguments.
+     * @param distance
+     * @param time
+     * @return fare
+     */
+    public double getFare(double distance, int time) {
+        double fare = distance * FARE_PER_KILOMETER + time * FARE_PER_MINUTE;
+        return fare;
+    }
+}
