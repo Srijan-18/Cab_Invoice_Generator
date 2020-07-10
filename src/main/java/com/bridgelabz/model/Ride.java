7f67aef31064fd1b57ef05c536dd376b6c79eb28
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 22:20:44 +0530

    ADDED: Computation of fares of rode according to category.

--- src/main/java/com/bridgelabz/model/Ride.java
+++ src/main/java/com/bridgelabz/model/Ride.java
@@ -1,12 +1,26 @@
 package com.bridgelabz.model;
 
 public class Ride {
+    /**
+     * ENUM to specify the ride category
+     */
+    public enum RideCategory {
+        PREMIUM,NORMAL;
+    }
 
+    public RideCategory rideCategory;
     public int time;
     public double distance;
 
-    public Ride(double distance, int time) {
+    /**
+     * CONSTRUCTOR to initialize the fields.
+     * @param distance
+     * @param time
+     * @param rideCategory
+     */
+    public Ride(double distance, int time, RideCategory rideCategory) {
         this.distance = distance;
         this.time = time;
+        this.rideCategory = rideCategory;
     }
 }
--- src/main/java/com/bridgelabz/service/FareCalculator.java
+++ src/main/java/com/bridgelabz/service/FareCalculator.java
@@ -4,30 +4,52 @@ import com.bridgelabz.model.Ride;
 
 public class FareCalculator {
 
-    private static final int FARE_PER_KILOMETER = 10;
-    private static final int FARE_PER_MINUTE = 1;
-    private static final double MINIMUM_FARE = 5;
+    private static int farePerKilometer;
+    private static int farePerMinute;
+    private static double minimumFare;
+
+    /**
+     * TASK: To Set Fares according to Ride Category
+     * @param rideCategory
+     */
+    private void setFaresAccordingTo(Ride.RideCategory rideCategory) {
+        switch (rideCategory) {
+            case NORMAL:
+                farePerKilometer = 10;
+                farePerMinute = 1;
+                minimumFare = 5;
+                break;
+            case PREMIUM:
+                farePerKilometer = 15;
+                farePerMinute = 2;
+                minimumFare = 20;
+                break;
+        }
+    }
 
     /**
      * TASK: To Generate and return fare based on distance and time passed as arguments.
      * @param distance
      * @param time
+     * @param rideCategory
      * @return fare
      */
-    private double getFare(double distance, int time) {
-            double fare = distance * FARE_PER_KILOMETER + time * FARE_PER_MINUTE;
-            return Math.max(MINIMUM_FARE, fare);
-            }
+    private double getFare(double distance, int time, Ride.RideCategory rideCategory) {
+        this.setFaresAccordingTo(rideCategory);
+        double fare = distance * farePerKilometer + time * farePerMinute;
+        return Math.max(minimumFare, fare);
+    }
 
     /**
      * TASK : To generate and return total fare based on details of rides taken.
+     *
      * @param rides
      * @return total fare
      */
     public double getTotalFare(Ride... rides) {
             double totalFare = 0.0;
             for (Ride ride:rides) {
-            totalFare += this.getFare(ride.distance, ride.time);
+            totalFare += this.getFare(ride.distance, ride.time, ride.rideCategory);
             }
             return totalFare;
     }
