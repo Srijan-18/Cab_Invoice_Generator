283856272d7b750f26c35ae160b61f3d4de189b1
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 20:23:28 +0530

    ADDED: Test case to validate minimum fare.

--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -1,8 +1,16 @@
 import com.bridgelabz.service.InvoiceGenerator;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabInvoiceTest {
+
+    InvoiceGenerator invoiceGenerator;
+
+    @Before
+    public void initializer() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
@@ -10,5 +18,10 @@ public class CabInvoiceTest {
         double distance = 2.0;
         int time = 1;
         Assert.assertEquals(21, invoiceGenerator.getFare(distance, time), 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        Assert.assertEquals(5, invoiceGenerator.getFare(0.1, 1), 0.0);
     }
 }
