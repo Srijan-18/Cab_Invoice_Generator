2d76e3290302cb1fb847f29d48310a8e418187e3
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 21:17:43 +0530

    ADDED: Methods to load map with Invoice Summaries accroding to userID and also to retrieve them repectively.

--- src/main/java/com/bridgelabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/service/InvoiceGenerator.java
@@ -3,7 +3,16 @@ package com.bridgelabz.service;
 import com.bridgelabz.model.InvoiceSummary;
 import com.bridgelabz.model.Ride;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InvoiceGenerator {
+
+    Map<String, InvoiceSummary> invoiceSummaryMap;
+
+    public InvoiceGenerator() {
+        this.invoiceSummaryMap = new HashMap<>();
+    }
 
     /**
      * TASK : To generate and return total fare based on details of rides taken.
@@ -22,5 +31,23 @@ public class InvoiceGenerator {
      */
     public InvoiceSummary getInvoiceSummary(Ride... rides) {
         return new InvoiceSummary(rides.length, this.getTotalFare(rides));
+    }
+
+    /**
+     * TASK: to load User specific Invoice in the Map with keys as UserIDs
+     * @param userID
+     * @param userRides
+     */
+    public void setUserSpecificInvoice(String userID, Ride... userRides) {
+        invoiceSummaryMap.put(userID, this.getInvoiceSummary(userRides));
+    }
+
+    /**
+     * TASK: To return Invoice Summary of given UserID.
+     * @param userID
+     * @return Invoice Summary
+     */
+    public InvoiceSummary getUserInvoiceSummary(String userID) {
+        return invoiceSummaryMap.get(userID);
     }
 }
