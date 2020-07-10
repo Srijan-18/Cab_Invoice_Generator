1c8a077b1db14b01a40c42346bce3837822fc783
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 20:44:34 +0530

    ADDED: Method to calculate total fare of multiple rides.

--- src/main/java/com/bridgelabz/model/Ride.java
+++ src/main/java/com/bridgelabz/model/Ride.java
@@ -1,4 +1,12 @@
 package com.bridgelabz.model;
 
 public class Ride {
+
+    public int time;
+    public double distance;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
 }
--- src/main/java/com/bridgelabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/service/InvoiceGenerator.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.model.Ride;
+
 public class InvoiceGenerator {
 
     private static final int FARE_PER_KILOMETER = 10;
@@ -7,7 +9,7 @@ public class InvoiceGenerator {
     private static final double MINIMUM_FARE = 5;
 
     /**
-     * TASK: To Generate and return based on distance and time passed as arguments.
+     * TASK: To Generate and return fare based on distance and time passed as arguments.
      * @param distance
      * @param time
      * @return fare
@@ -15,5 +17,18 @@ public class InvoiceGenerator {
     public double getFare(double distance, int time) {
         double fare = distance * FARE_PER_KILOMETER + time * FARE_PER_MINUTE;
         return Math.max(MINIMUM_FARE, fare);
+    }
+
+    /**
+     * TASK : To generate and return total fare based on details of rides taken.
+     * @param rides
+     * @return total fare
+     */
+    public double getTotalFare(Ride... rides) {
+        double totalFare = 0.0;
+        for (Ride ride:rides) {
+            totalFare += this.getFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
