b031a82ac85321382bf8b800fead2130d86c6fdf
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-13 10:35:23 +0530

    REFACTORED: Test cases to accomodate changes made in main code.

--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -4,6 +4,7 @@ import com.bridgelabz.service.InvoiceGenerator;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import utility.RideCategory;
 
 public class CabInvoiceTest {
 
@@ -19,37 +20,39 @@ public class CabInvoiceTest {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 1;
-        Assert.assertEquals(21, invoiceGenerator.getTotalFare(new Ride(distance, time, Ride.RideCategory.NORMAL)), 0.0);
+        Assert.assertEquals(21, invoiceGenerator.getTotalFare(new Ride(distance, time,
+                                                                                RideCategory.NORMAL)), 0.0);
     }
 
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
-        Assert.assertEquals(5, invoiceGenerator.getTotalFare(new Ride(0.1, 1, Ride.RideCategory.NORMAL)),
+        Assert.assertEquals(5, invoiceGenerator.getTotalFare(new Ride(0.1, 1,
+                                                                                        RideCategory.NORMAL)),
                             0.0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
-        Ride[] rides = { new Ride(2.0, 1, Ride.RideCategory.NORMAL),
-                         new Ride(0.1, 1, Ride.RideCategory.NORMAL)};
+        Ride[] rides = { new Ride(2.0, 1, RideCategory.NORMAL),
+                         new Ride(0.1, 1, RideCategory.NORMAL)};
         Assert.assertEquals(26, invoiceGenerator.getTotalFare(rides), 0.0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-        Ride[] rides = { new Ride(2.0, 1, Ride.RideCategory.NORMAL),
-                        new Ride(0.1, 1, Ride.RideCategory.NORMAL)};
+        Ride[] rides = { new Ride(2.0, 1, RideCategory.NORMAL),
+                        new Ride(0.1, 1, RideCategory.NORMAL)};
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 26);
         Assert.assertEquals(expectedInvoiceSummary, invoiceGenerator.getInvoiceSummary(rides));
     }
 
     @Test
     public void givenUserId_ShouldReturnInvoiceOfGivenUserId() {
-        Ride[] user1Rides = { new Ride(3.0, 2, Ride.RideCategory.NORMAL),
-                              new Ride(0.1, 1, Ride.RideCategory.NORMAL)};
+        Ride[] user1Rides = { new Ride(3.0, 2, RideCategory.NORMAL),
+                              new Ride(0.1, 1, RideCategory.NORMAL)};
         invoiceGenerator.setUserSpecificInvoice("User1", user1Rides);
-        Ride []user2Rides = { new Ride(5.0, 2, Ride.RideCategory.NORMAL),
-                              new Ride(6.1, 3, Ride.RideCategory.NORMAL)};
+        Ride []user2Rides = { new Ride(5.0, 2, RideCategory.NORMAL),
+                              new Ride(6.1, 3, RideCategory.NORMAL)};
         invoiceGenerator.setUserSpecificInvoice("User2", user2Rides);
         Assert.assertEquals(new InvoiceSummary(2,37), invoiceGenerator
                                                                     .getUserInvoiceSummary("User1"));
@@ -57,11 +60,11 @@ public class CabInvoiceTest {
 
     @Test
     public void givenRideCategory_SelectivelyPREMIUM_ShouldReturnFareAccordingly() {
-        Ride[] user1Rides = { new Ride(3.0, 2, Ride.RideCategory.NORMAL),
-                              new Ride(0.1, 1, Ride.RideCategory.PREMIUM)};
+        Ride[] user1Rides = { new Ride(3.0, 2, RideCategory.NORMAL),
+                              new Ride(0.1, 1, RideCategory.PREMIUM)};
         invoiceGenerator.setUserSpecificInvoice("User1", user1Rides);
-        Ride []user2Rides = { new Ride(5.0, 2, Ride.RideCategory.PREMIUM),
-                              new Ride(6.1, 3, Ride.RideCategory.NORMAL)};
+        Ride []user2Rides = { new Ride(5.0, 2, RideCategory.PREMIUM),
+                              new Ride(6.1, 3, RideCategory.NORMAL)};
         invoiceGenerator.setUserSpecificInvoice("User2", user2Rides);
         Assert.assertEquals(new InvoiceSummary(2,52), invoiceGenerator
                                                                     .getUserInvoiceSummary("User1"));
